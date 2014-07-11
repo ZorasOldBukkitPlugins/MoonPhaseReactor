0324c24b3b98853392377c6f5159a2186c3772a0
Zora <MrZoraman@Gmail.com>
2014-07-10 17:40:59 -0700

    Attached player manager to the bukkit even system

--- src/com/lagopusempire/moonphasereactor/MoonPhaseReactor.java
+++ src/com/lagopusempire/moonphasereactor/MoonPhaseReactor.java
@@ -11,9 +11,15 @@ public class MoonPhaseReactor extends JavaPlugin
 {
     private static final int MOON_PHASE_CHECK_INTERVAL = 20;//every 10 seconds
     
+    private PlayerManager playerManager;
+    
     @Override
     public void onEnable()
     {
+        playerManager = new PlayerManager();
+        
+        getServer().getPluginManager().registerEvents(playerManager, this);
+        
         getServer().getScheduler().runTaskTimer(this, new MoonPhaseEventTriggerer(), 0, MOON_PHASE_CHECK_INTERVAL);
     }
 }
